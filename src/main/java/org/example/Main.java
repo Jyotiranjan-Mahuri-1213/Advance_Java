8a15e8c80eddc4d6710b22603a24ca4df6e98612
Jyotiranjan-Mahuri-1213 <jyotiranjanmahuri@gmail.com>
2026-07-13 15:41:10 +0530

    JDBC completed step 3,4,5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,43 @@
 package org.example;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-import static java.lang.Class.forName;
+import java.sql.*;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-   public static void main(String[] args) {
-       String URL="jdbc:mysql://localhost:3306/advance_java";
-       String USER="root";
-       String PASSWORD="Jyoti@2004";
-       try{
-           Class .forName("com.mysql.cj.jdbc.Driver");
-           DriverManager .getConnection(URL,USER,PASSWORD);
-       } catch (ClassNotFoundException | SQLException e) {
-           throw new RuntimeException(e);
-       }
-    System.out.println("now jdbc is connected");
-   }
-    }
+    public static void main(String[] args) {
+        String URL ="jdbc:mysql://localhost:3306/advance_java";
+        String USER = "root";
+        String PASSWORD = "Jyoti@2004";
+        try {
 
+            Connection con = DriverManager.getConnection(URL,USER,PASSWORD);
+
+            PreparedStatement pr=con.prepareStatement("insert into student_data values (?,?,?)");
+            System.out.println("Enter id  : \n name :\n Domain :");
+            Scanner sc = new Scanner(System.in);
+
+
+            pr.setInt(1,sc.nextInt());
+            sc.nextLine();
+            pr.setString(2,sc.nextLine());
+            pr.setString(3,sc.nextLine());
+
+            //4th Step(Execute Query)
+            int rowsaffect = pr.executeUpdate();
+            System.out.println(rowsaffect);
+            if(rowsaffect>0){
+                System.out.println("Data inserted");
+            }
+            con.close();
+
+
+
+        } catch ( SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
