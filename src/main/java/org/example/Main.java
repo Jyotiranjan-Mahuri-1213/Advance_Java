7a4020f38b3f55dfe6860b25b4129164f506f589
Jyotiranjan-Mahuri-1213 <jyotiranjanmahuri@gmail.com>
2026-08-03 23:36:20 +0530

    updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,41 +4,134 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
+
+        TrainDAO trainDAO = new TrainDAO();
 
         Scanner sc = new Scanner(System.in);
 
+        int choice;
 
+        do {
 
+            System.out.println("\n================================");
+            System.out.println(" RAILWAY TICKET BOOKING SYSTEM");
+            System.out.println("================================");
 
+            System.out.println("1. Add Train");
+            System.out.println("2. View All Trains");
+            System.out.println("3. Update Train");
+            System.out.println("4. Delete Train");
+            System.out.println("5. Search Train");
+            System.out.println("6. Book Ticket");
+            System.out.println("7. View All Bookings");
+            System.out.println("8. Exit");
 
-                System.out.print("\nEnter your choice: ");
+            System.out.print("\nEnter your choice: ");
+
             choice = sc.nextInt();
+
             switch (choice) {
+
                 case 1:
 
+                    Train train = new Train();
 
+                    System.out.print("Enter train number: ");
+                    train.trainNumber = sc.next();
 
                     sc.nextLine();
 
+                    System.out.print("Enter train name: ");
+                    train.trainName = sc.nextLine();
 
+                    System.out.print("Enter source: ");
+                    train.source = sc.nextLine();
 
+                    System.out.print("Enter destination: ");
+                    train.destination = sc.nextLine();
 
+                    System.out.print("Enter total seats: ");
+                    train.totalSeats = sc.nextInt();
+
+                    train.availableSeats =
+                            train.totalSeats;
+
+                    System.out.print("Enter ticket price: ");
+                    train.ticketPrice = sc.nextDouble();
+
+                    trainDAO.addTrain(train);
 
                     break;
+
                 case 2:
+
+                    System.out.println(
+                            "View All Trains"
+                    );
+
                     break;
+
                 case 3:
+
+                    System.out.println(
+                            "Update Train"
+                    );
+
                     break;
 
                 case 4:
+
+                    System.out.println(
+                            "Delete Train"
+                    );
+
                     break;
 
                 case 5:
 
-                default:
-            }
-        }
+                    System.out.println(
+                            "Search Train"
+                    );
 
+                    break;
+
+                case 6:
+
+                    System.out.println(
+                            "Book Ticket"
+                    );
+
+                    break;
+
+                case 7:
+
+                    System.out.println(
+                            "View All Bookings"
+                    );
+
+                    break;
+
+                case 8:
+
+                    System.out.println(
+                            "!!!! Thank you. Exiting !!!!"
+                    );
+
+                    break;
+
+                default:
+
+                    System.out.println(
+                            "Invalid choice. Please try again."
+                    );
+            }
+
+        } while (choice != 8);
+
+        sc.close();
     }
+
+
 }
